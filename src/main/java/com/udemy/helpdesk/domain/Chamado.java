f986e2dcfb0e74061dbfca4cb854e0ef8020bab7
Fabio <110238114+Femtokiller@users.noreply.github.com>
2026-03-25 19:00:42 -0300

    Anotacoes de jpa para classes de dominio

--- src/main/java/com/udemy/helpdesk/domain/Chamado.java
+++ src/main/java/com/udemy/helpdesk/domain/Chamado.java
@@ -1,21 +1,46 @@
 package com.udemy.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.udemy.helpdesk.domain.enums.Prioridade;
 import com.udemy.helpdesk.domain.enums.Status;
 
-public class Chamado 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Chamado implements Serializable
 {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataAbertura = LocalDate.now();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento = LocalDate.now();
+	
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
+	private Tecnico tecnico;
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
+	private Cliente cliente;
+	
 	private Prioridade prioridade;
 	private Status status;
 	private String titulo;
 	private String observacoes;
-	private Tecnico tecnico;
-	private Cliente cliente;
 	
 	public Chamado(Integer id, Prioridade prioridade, Status status, String titulo, String observacoes, Tecnico tecnico,
 			Cliente cliente) {
